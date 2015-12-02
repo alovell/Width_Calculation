c5a06bb4314401effb592a1653049f0dbe0f82a3
alovell <lovellam@msu.edu>
2015-12-02 10:50:14 -0500

    Fixed Simpson's Rule

--- simpson.f90
+++ simpson.f90
@@ -1,14 +1,8 @@
-   !subroutine simpsons(ps,cwf,pots,energy,ke,hbc,mu,file,cn,N,nchan)
    subroutine simpsons(ps,nchan)
    use constants
    use channels
    use totwf
    implicit none
-   !integer, intent(in) :: cn,N,nchan
-   !real*8, intent(in) :: hbc,energy,mu,ke
-   !real*8, intent(in) :: cwf(N,nchan+1)
-   !real*8, intent(in) :: pots(400)
-   !character (len=10), intent(in) :: file
    integer, intent(in) :: nchan
    real*8, intent(inout) :: ps
    integer i,j,ifail,m1
@@ -33,16 +27,13 @@
    ! for whatever reason
    ! value is put into fc(2)
    
-   ! calculate momentum from energy
-   !ke=sqrt(mu*energy)/hbc
-   
-   do i=1,400-2 ! shorter when the potential is read in b/c U(r->inf)=0
+   do i=1,400-2,3 ! shorter when the potential is read in b/c U(r->inf)=0
       do j=1,3
          call coulfg(ke*chi(i+j-1,1),0d0,L,L,fc,gc,fcp,gcp,1,0,ifail,m1)
          fn(j) = fc(2)*pots(i)*chi(i+j-1,channum+1)/chi(i+j-1,1)
 	 !print *, fc(2)
       enddo 
-      ps = ps + ((chi(i+1,1)-chi(i,1))/3.d0)*(fn(1)+4*fn(2)+2*fn(3))
+      ps = ps + ((chi(i+1,1)-chi(i,1))/3.d0)*(fn(1)+4*fn(2)+fn(3))
    enddo 
    
    !ps=0
